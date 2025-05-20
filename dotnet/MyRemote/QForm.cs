75c860fb3afbc25d0714f27be594f18ebfdcf4f3
Lee Goddard <leegee@gmail.com>
2025-05-20 10:40:44 +0200

    QRCode size

--- dotnet/MyRemote/QForm.cs
+++ dotnet/MyRemote/QForm.cs
@@ -12,18 +12,18 @@ public class QRForm : Form
         MaximizeBox = false;
         MinimizeBox = false;
         StartPosition = FormStartPosition.CenterScreen;
-        ClientSize = new Size(350, 350);
 
         var qrGenerator = new QRCodeGenerator();
-        var qrData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+        var qrData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.M);
         var qrCode = new QRCode(qrData);
-        var qrImage = qrCode.GetGraphic(20);
+        var qrImage = qrCode.GetGraphic(10);
+        ClientSize = new Size(350, 350);
 
         var pictureBox = new PictureBox
         {
             Dock = DockStyle.Fill,
             Image = qrImage,
-            SizeMode = PictureBoxSizeMode.CenterImage
+            SizeMode = PictureBoxSizeMode.Zoom
         };
 
         Controls.Add(pictureBox);
